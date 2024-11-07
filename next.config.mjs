fe76da27d96ef189674936b60c3a8d23f8eb8a94
jurij <jurij.kolenik@gmail.com>
2024-11-07 09:20:57 +0100

    Added asset prefix in nestjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.BACKEND_URL || '',
+    basePath: process.env.FRONTEND_URL || '',
+    assetPrefix: process.env.FRONTEND_URL || '',
     reactStrictMode: true,
     i18n: {
         locales: ["en"],
