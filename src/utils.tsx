cd62770ec282fc350c83924345fb0f90a1a6e14e
RobertVampel <robert.vampel@3appes.com>
2025-02-19 11:46:28 +0100

    copyToClipboard fix

--- src/utils.tsx
+++ src/utils.tsx
@@ -54,7 +54,16 @@ export const truncateString = (text: string, from: number = 4, to: number = 4) =
     return `${text.substring(0, from)}...${text.substring(text.length - to, text.length)}`;
 }
 export const copyToClipboard = (text: string) => {
-    navigator.clipboard.writeText(text)
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(text);
+    } else {
+        const textarea = document.createElement('textarea');
+        textarea.value = text;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+    }
 }
 
 export const isNumeric = (value: string | number | boolean | undefined | null) => {
